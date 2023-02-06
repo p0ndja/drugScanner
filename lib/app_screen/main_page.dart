257dda8b6028e56f423e30e05792fd3e96202912
PondJaᵀᴴ <18050176+p0ndja@users.noreply.github.com>
2023-02-07 01:19:33 +0700

    Resolved all 'fordcer' branch conflict

---
+++ lib/app_screen/main_page.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:drug_scanner/app_screen/account.dart';
+import 'package:drug_scanner/app_screen/search.dart';
+import 'package:drug_scanner/app_screen/home.dart';
+
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({super.key});
+
+  @override
+  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  int _selectedIndex = 0;
+  static const List<Widget> _widgetOptions = <Widget>[
+    HomeScreen(),
+    SearchScreen(),
+    UserScreen(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('DrugScanner'),
+          automaticallyImplyLeading: false,
+        ),
+        body: Center(
+          child: _widgetOptions.elementAt(_selectedIndex),
+        ),
+        bottomNavigationBar: bottomNavigator(),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+        floatingActionButton: FloatingActionButton.large(
+          shape: const CircleBorder(),
+          onPressed: () => {_onItemTapped(1)},
+          child: const Icon(Icons.camera_alt),
+        ));
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  Widget bottomNavigator() {
+    return BottomNavigationBar(
+      items: const <BottomNavigationBarItem>[
+        BottomNavigationBarItem(
+          icon: Icon(Icons.search),
+          label: 'Search',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.camera_alt),
+          label: '',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.person),
+          label: 'User',
+        ),
+      ],
+      currentIndex: _selectedIndex,
+      selectedItemColor: Colors.teal,
+      onTap: _onItemTapped,
+    );
+  }
+}
